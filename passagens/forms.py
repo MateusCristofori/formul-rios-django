9cf30143e47bd3e9e0f99ef9e37bf4f4a9823f9d
Mateus Cristofori <mateuscristofori@gmail.com>
2022-09-22 16:22:14 -0300

    Mais campos de formulário

--- passagens/forms.py
+++ passagens/forms.py
@@ -1,10 +1,20 @@
-from ssl import Options
+from cProfile import label
+from datetime import datetime
 from django import forms
 from tempus_dominus.widgets import DatePicker
-
+from .classe_viagem import tipos_de_classes
 
 class PassagemForm(forms.Form):
   origem = forms.CharField(label='Origem', max_length=100)
   destino = forms.CharField(label='Destino', max_length=100)
+  email = forms.EmailField(label='E-mail', max_length=100)
   data_ida = forms.DateField(label='Ida', widget=DatePicker())
   data_volta = forms.DateField(label='Volta', widget=DatePicker())
+  data_pesquisa = forms.DateField(label="Data da pesquisa", disabled=True, initial=datetime.today)
+  classe_viagem = forms.ChoiceField(label='Classe do voo', choices=tipos_de_classes)
+  informacoes = forms.CharField(
+    label='Informações extras',
+    max_length=200,
+    widget=forms.Textarea(),
+    required=False
+  )
